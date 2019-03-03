3e7b4df918bb67042bb20d9f2aeb55dce3f110e3
Linus Klöckner <linus.kloeckner@googlemail.com>
2019-03-03 20:05:32 +0100

    Change youtube_decipher dependency to stable branch

--- conanfile.py
+++ conanfile.py
@@ -14,7 +14,7 @@ class YoutubeListDownloaderConan(ConanFile):
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = "shared=False", "fPIC=True"
     requires = "boost/1.66.0@conan/stable", "cpr/1.3.0@linux13524/stable", \
-               "youtube_decipher/1.0.1@linux13524/testing", "sqlitecpp/2.2.0@bincrafters/stable"
+               "youtube_decipher/1.0.1@linux13524/stable", "sqlitecpp/2.2.0@bincrafters/stable"
     generators = "cmake"
     exports_sources = "package/*"
 
