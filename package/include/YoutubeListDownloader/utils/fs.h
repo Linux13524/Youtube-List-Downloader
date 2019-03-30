46fb4328502741c9a3963936bf0032cadd905724
Linus Klöckner <linus.kloeckner@googlemail.com>
2019-03-30 13:26:02 +0100

    Add filesystem settings
    Add database path to filesystem settings

--- package/include/YoutubeListDownloader/utils/fs.h
+++ package/include/YoutubeListDownloader/utils/fs.h
@@ -3,6 +3,7 @@
 #ifndef INCLUDE_UTILS_FS_H_
 #define INCLUDE_UTILS_FS_H_
 
+#include <boost/filesystem.hpp>
 #include <boost/regex.hpp>
 #include <string>
 
@@ -18,6 +19,25 @@ namespace Filesystem {
             *p_string = boost::regex_replace(*p_string, expr, "");
         }
     };
-} // namespace Filesystem
 
-#endif // INCLUDE_UTILS_FS_H_
+    class Settings {
+     public:
+        static Settings& Instance() {
+            static Settings s_instance;
+            return s_instance;
+        }
+
+        inline void SetDbPath(const boost::filesystem::path& p_db_path) {
+            m_current_db_path = p_db_path;
+        }
+
+        inline boost::filesystem::path GetDbPath() { return m_current_db_path; }
+
+     private:
+        Settings() = default;
+
+        boost::filesystem::path m_current_db_path{boost::filesystem::current_path()};
+    };
+}   // namespace Filesystem
+
+#endif   // INCLUDE_UTILS_FS_H_
